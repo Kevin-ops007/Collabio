2e8824d8adb8f5a21292a466f1eb3ac93c58a3aa
Sumeir Khinda <sumeir.khinda@gmail.com>
2021-11-22 00:36:50 -0800

    did a lot of shit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+     return (
+        <ChatEngine 
+            height="100vh"
+            projectID="4fb8006d-3600-47d6-8f5a-cc41e2c4b299"
+            username="admin"
+            userSecret="123123"
+
+        />
+     )
+}
+
+export default App;
